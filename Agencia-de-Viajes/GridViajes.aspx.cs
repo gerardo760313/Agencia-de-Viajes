da47ccb2631e14fcb76dfd10604f5e7d4949e633
miguel1981 <jm_ramirezv@hotmail.com>
2015-11-22 18:50:15 -0600

    grid
    Avance

--- Agencia-de-Viajes/GridViajes.aspx.cs
+++ Agencia-de-Viajes/GridViajes.aspx.cs
@@ -19,8 +19,8 @@ public partial class GridViajes : System.Web.UI.Page
         }
         catch (Exception ex)
         {
-            
-            Title = "Error: "+ex.Message;
+
+            Title = "Error: " + ex.Message;
         }
     }
 
@@ -30,7 +30,7 @@ public partial class GridViajes : System.Web.UI.Page
         {
             List<EntDestino> listDest = new BusDestino().obtDestino();
             DropDownList ddlDest = (DropDownList)gvViajes.FooterRow.FindControl("ddlDestinoFT");
-            
+
             ddlDest.DataSource = listDest;
             ddlDest.DataTextField = "nombre";
             ddlDest.DataValueField = "id";
@@ -39,8 +39,8 @@ public partial class GridViajes : System.Web.UI.Page
         }
         catch (Exception ex)
         {
-            
-            Title = "Error: "+ex.Message;
+
+            Title = "Error: " + ex.Message;
         }
     }
 
@@ -59,8 +59,8 @@ public partial class GridViajes : System.Web.UI.Page
         }
         catch (Exception ex)
         {
-            
-            Title = "Error: "+ex.Message;
+
+            Title = "Error: " + ex.Message;
         }
     }
 
@@ -77,11 +77,25 @@ public partial class GridViajes : System.Web.UI.Page
             Title = "Error: " + ex.Message;
         }
     }
-   
+
     protected void lnbAgregar_Click(object sender, EventArgs e)
     {
-        EntViaje entViaje = new EntViaje();
-        entViaje.nombre = ((TextBox)gvViajes.FooterRow.FindControl("txtNombreFT")).Text ;
+        try
+        {
+            EntViaje entViaje = new EntViaje();
+            
+            entViaje.nombre = ((TextBox)gvViajes.FooterRow.FindControl("txtNombreFT")).Text;
+            entViaje.fechaLlegada = Convert.ToDateTime(((TextBox)gvViajes.FooterRow.FindControl("txtLlegadaEIT")).Text);
+            entViaje.categoriaId = Convert.ToInt32(((TextBox)gvViajes.FooterRow.FindControl("ddlCategoriaFT")).Text);
+
+            new BusViaje().Insertar(entViaje);
+            Response.Redirect("Default.aspx");
+        }
+        catch (Exception ex)
+        {
+            
+            Title = "Error: "+ex.Message;
+        }
     }
     protected void gvViajes_RowDeleting(object sender, GridViewDeleteEventArgs e)
     {
@@ -94,12 +108,56 @@ public partial class GridViajes : System.Web.UI.Page
         catch (Exception ex)
         {
 
-            Title = "Error: " + ex.Message ;
+            Title = "Error: " + ex.Message;
         }
     }
     protected void gvViajes_RowUpdating(object sender, GridViewUpdateEventArgs e)
     {
+        String path = Server.MapPath("img\\");
+        path = path + DateTime.Now.ToString("HHmm");
 
+        try
+        {
+            EntViaje ent = new EntViaje();
+            
+            ent.fotoLugar = gvViajes.DataKeys[e.RowIndex].Values["fotoLugar"].ToString();
+            ent.fotoHotel = gvViajes.DataKeys[e.RowIndex].Values["fotoHotel"].ToString();
+
+            FileUpload fuFotoLug = (FileUpload)gvViajes.Rows[e.RowIndex].FindControl("fuFotoLugarEIT");
+            FileUpload fuFotoHot = (FileUpload)gvViajes.Rows[e.RowIndex].FindControl("fuFotoHotelEIT");
+            if (fuFotoLug.HasFile)
+            {
+                string fileName = fuFotoLug.FileName;
+                ent.fotoLugar = path + fileName;
+                fuFotoLug.SaveAs(ent.fotoLugar);
+                ent.fotoLugar = "img/" + DateTime.Now.ToString("HHmm") + fileName;
+            }
+            if (fuFotoHot.HasFile)
+            {
+                string fileName = fuFotoHot.FileName;
+                ent.fotoHotel = path + fileName;
+                fuFotoHot.SaveAs(ent.fotoHotel);
+                ent.fotoHotel = "img" + DateTime.Now.ToString("HHmm") + fileName;
+            }
+
+            ent.id = Convert.ToInt32( gvViajes.DataKeys[e.RowIndex].Values["Id"]);
+            ent.nombre = ((TextBox)gvViajes.Rows[e.RowIndex].FindControl("txtNombreEIT")).Text;
+            ent.fechaLlegada = Convert.ToDateTime(((TextBox)gvViajes.Rows[e.RowIndex].FindControl("txtLlegadaEIT")).Text);
+            ent.categoriaId = Convert.ToInt32 (((DropDownList)gvViajes.Rows[e.RowIndex].FindControl("ddlCategoriaEIT")).SelectedItem.Value);
+            ent.destinoId = Convert.ToInt32(((DropDownList)gvViajes.Rows[e.RowIndex].FindControl("ddlDestinoEIT")).SelectedItem.Value);
+            ent.descripcion = ((TextBox)gvViajes.Rows[e.RowIndex].FindControl("txtDescrEIT")).Text;
+            ent.video = ((TextBox)gvViajes.Rows[e.RowIndex].FindControl("txtVideoEIT")).Text;
+            ent.estatus = ((CheckBox)gvViajes.Rows[e.RowIndex].FindControl("ckbEstatusEIT")).Checked;
+
+            new BusViaje().Actualizar(ent);
+            Response.Redirect("Default.aspx");
+
+        }
+        catch (Exception ex)
+        {
+
+            Title = ex.Message;
+        }
     }
     protected void gvViajes_RowEditing(object sender, GridViewEditEventArgs e)
     {
@@ -129,7 +187,7 @@ public partial class GridViajes : System.Web.UI.Page
         }
         catch (Exception ex)
         {
-            Title = "Error: "+ex.Message;
+            Title = "Error: " + ex.Message;
         }
 
     }
@@ -144,8 +202,8 @@ public partial class GridViajes : System.Web.UI.Page
         }
         catch (Exception ex)
         {
-            
-            Title = "Error: "+ex.Message;
+
+            Title = "Error: " + ex.Message;
         }
     }
 }
